cd251fed7b08f1de98343ed53f141fb9431309a8
Ben Kinard <benjaminjameskinard@gmail.com>
2022-03-11 15:15:54 -0700

    Update home page to use Vue

---
+++ website/js/home.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		candidates: [{
+			rank: 1,
+			name: "Nikola Jokic",
+			image: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+		}, {
+			rank: 2,
+			name: "Joel Embiid",
+			image: "https://cdn.nba.com/headshots/nba/latest/1040x760/203954.png"
+		}, {
+			rank: 3,
+			name: "Giannis Antetokounmpo",
+			image: "https://cdn.nba.com/headshots/nba/latest/1040x760/203507.png"
+		}],
+	},
+});
