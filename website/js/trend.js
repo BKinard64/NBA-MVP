24f72041383ded26f45a44b702f8309535aaaa03
Ben Kinard <benjaminjameskinard@gmail.com>
2022-03-11 15:59:29 -0700

    Update trends page to use Vue

---
+++ website/js/trend.js
@@ -0,0 +1,120 @@
+var page = new Vue({
+	el: '#page',
+	data: {
+		months: [{
+			name: "November",
+			weeks: [{
+				number: 1,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}, {
+				number: 2,
+				playerName: "Joel Embiid",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203954.png"
+			}, {
+				number: 3,
+				playerName: "Giannis Antetokounmpo",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203507.png"
+			}, {
+				number: 4,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}]
+		}, {
+			name: "December",
+			weeks: [{
+				number: 1,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}, {
+				number: 2,
+				playerName: "Joel Embiid",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203954.png"
+			}, {
+				number: 3,
+				playerName: "Giannis Antetokounmpo",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203507.png"
+			}, {
+				number: 4,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}]
+		}, {
+			name: "January",
+			weeks: [{
+				number: 1,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}, {
+				number: 2,
+				playerName: "Joel Embiid",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203954.png"
+			}, {
+				number: 3,
+				playerName: "Giannis Antetokounmpo",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203507.png"
+			}, {
+				number: 4,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}]
+		}, {
+			name: "February",
+			weeks: [{
+				number: 1,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}, {
+				number: 2,
+				playerName: "Joel Embiid",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203954.png"
+			}, {
+				number: 3,
+				playerName: "Giannis Antetokounmpo",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203507.png"
+			}, {
+				number: 4,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}]
+		}, {
+			name: "March",
+			weeks: [{
+				number: 1,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}, {
+				number: 2,
+				playerName: "Joel Embiid",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203954.png"
+			}, {
+				number: 3,
+				playerName: "Giannis Antetokounmpo",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203507.png"
+			}, {
+				number: 4,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}]
+		}, {
+			name: "April",
+			weeks: [{
+				number: 1,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}, {
+				number: 2,
+				playerName: "Joel Embiid",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203954.png"
+			}, {
+				number: 3,
+				playerName: "Giannis Antetokounmpo",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203507.png"
+			}, {
+				number: 4,
+				playerName: "Nikola Jokic",
+				playerImage: "https://cdn.nba.com/headshots/nba/latest/1040x760/203999.png"
+			}]
+		}],
+	},
+});
